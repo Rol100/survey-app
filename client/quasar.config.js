01b33182cb0ca18079eb750d7ef4a9ee1c9e4c2f
Bil Valerio <bilv2789@nycstudents.net>
2024-02-01 17:07:56 -0500

    Student Survey client is complete

--- client/quasar.config.js
+++ client/quasar.config.js
@@ -12,7 +12,7 @@
 const { configure } = require('quasar/wrappers');
 
 
-module.exports = configure(function (/* ctx */) {
+module.exports = configure(function ( ctx ) {
   return {
     eslint: {
       // fix: true,
@@ -60,7 +60,15 @@ module.exports = configure(function (/* ctx */) {
         node: 'node20'
       },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
+      env: {
+        SERVER_URL: ctx.dev ? 'http://localhost:3000' : 'http://<YOUR_PROD_IP>',
+        CREATOR_NAME: 'Bil Valerio',
+        CREATOR_EMAIL: 'bilvaleio57@gmail.com',
+        LINKEDIN: 'https://www.linkedin.com/in/bilvalerio/'
+      },
+      distDir: '../server/public'
+ 
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
@@ -86,7 +94,11 @@ module.exports = configure(function (/* ctx */) {
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
-    devServer: {
+    devServer: { htmlVariables: {
+      productName: 'BAHS Student Job Survey App',
+      productDescription: 'The Bronx Aerospace High School Student Survey App collects job preferences from high school students.'
+    },
+ 
       // https: true
       open: true // opens browser window automatically
     },
@@ -106,7 +118,9 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Notify'
+      ]
     },
 
     // animations: 'all', // --- includes all animations
