9cdf17eb207b2453803b58840ff341225487f911
Bil Valerio <bilv2789@nycstudents.net>
2024-02-06 14:59:11 -0500

    Updated PROD URL in quasar.config.js

--- client/quasar.config.js
+++ client/quasar.config.js
@@ -62,7 +62,7 @@ module.exports = configure(function ( ctx ) {
 
       vueRouterMode: 'history', // available values: 'hash', 'history'
       env: {
-        SERVER_URL: ctx.dev ? 'http://localhost:3000' : 'http://<YOUR_PROD_IP>',
+        SERVER_URL: ctx.dev ? 'http://localhost:3000' : 'http://54.196.216.230',
         CREATOR_NAME: 'Bil Valerio',
         CREATOR_EMAIL: 'bilvaleio57@gmail.com',
         LINKEDIN: 'https://www.linkedin.com/in/bilvalerio/'
